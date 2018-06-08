63ce22b614a533cc0f5f952222556ec84a788546
Andreas Ritter <azuri.ritter@gmail.com>
2018-06-08 17:15:51 +0800

    Add ACID's program to read Gaia data

---
+++ python/readGaia.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+import csv
+import csvData
+import csvFree
+
+fname = '/Volumes/external/azuri/data/gaia/GaiaSource_1008431284282695936_1008626993058019840.csv'
+
+def readGaiaToDict():
+    reader = csv.reader(open(fname, 'r'))
+    d = {}
+
+    iLine = 0
+    keys = []
+    values = []
+    for row in reader:
+        if iLine == 0:
+            keys = row
+            print len(keys)
+        else:
+            values.append(row)
+        iLine += 1
+    print len(values)
+    print values[0]
+
+    data = {}
+    for i in range(len(keys)):
+        data[keys[i]] = [value[i] for value in values]
+
+    print 'data[',keys[0],'] = ',data[keys[0]]
+    print 'data[ra] = ',data['ra']
+
+def readGaiaCSVData():
+    return csvFree.readCSVFile(fname)
+
+def main():
+    data = readGaiaCSVData()
+    print 'size = ',data.size()
+    print data.getData('ra')
+
+main()
