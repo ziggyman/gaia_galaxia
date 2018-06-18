6268ada6cabfa127800ce2f3ffb8bd5cf8ee9aba
Andreas Ritter <azuri.ritter@gmail.com>
2018-06-18 13:06:11 +0800

    Parse galcomp.getStarsInXYWindow

--- python/galcomp.cc
+++ python/galcomp.cc
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "../cpp/galcomp.h"
+#include "../cpp/csvData.h"
 
 namespace py = pybind11;
 
@@ -8,4 +9,5 @@ PYBIND11_MODULE(galcomp, m) {
     m.doc() = "pybind11 galcomp plugin"; // optional module docstring
 
     m.def("countStars", &countStars);
+    m.def("getStarsInXYWindow", (CSVData (*)(vector<Pixel> const&, Pixel const&, string const&)) &getStarsInXYWindow);
 }
